d8a284f83aa5507777457ff828d461ceb409b286
cocoder16 <cocoder16@gmail.com>
2022-01-07 16:59:10 +0900

    [Add] MUI index

---
+++ src/components/MUI/index.ts
@@ -0,0 +1,9 @@
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import Checkbox from "@mui/material/Checkbox";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+
+export { Button, Modal, Checkbox, AppBar, Toolbar, BottomNavigation, BottomNavigationAction };
