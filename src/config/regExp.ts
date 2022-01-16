1e96a6c81b090a858085070a235ba4fdb4730d2e
cocoder16 <cocoder16@gmail.com>
2022-01-16 16:47:08 +0900

    [Fix] regExp of phoneNumber

--- src/config/regExp.ts
+++ src/config/regExp.ts
@@ -2,7 +2,7 @@ const regExp = {
   email: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
   password: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=])[A-Za-z\d!@#$%^&*()_\-+=]{8,}$/,
   userName: /^[가-힣a-zA-Z]{1,5}$/,
-  phoneNumber: /^[0-9]{11}$/,
+  phoneNumber: /^01([0|1|6|7|8|9])?([0-9]{3,4})?([0-9]{4})$/,
 };
 
 export default regExp;
