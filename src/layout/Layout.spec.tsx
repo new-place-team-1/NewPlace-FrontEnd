a9e66755aef660256a245011441e986a248b60ed
cocoder16 <cocoder16@gmail.com>
2022-01-25 15:26:30 +0900

    [Refactor] rename test

--- src/layout/Layout.spec.tsx
+++ src/layout/Layout.spec.tsx
@@ -4,7 +4,7 @@ import Layout from ".";
 import { viewportSizeForTest } from "src/config/device";
 
 describe("Layout", () => {
-  it("When viewport width <= 992, Then render MobileHeader", () => {
+  it("When viewport width <= 992, Then render BottomMenu", () => {
     cy.viewport(viewportSizeForTest.mobile.width, viewportSizeForTest.mobile.height);
 
     mount(<Layout />);
@@ -12,7 +12,7 @@ describe("Layout", () => {
     cy.get("#bottom-menu");
   });
 
-  it("When viewport width > 992, Then render DesktopHeader", () => {
+  it("When viewport width > 992, Then not render BottomMenu", () => {
     cy.viewport(viewportSizeForTest.desktop.width, viewportSizeForTest.desktop.height);
 
     mount(<Layout />);
