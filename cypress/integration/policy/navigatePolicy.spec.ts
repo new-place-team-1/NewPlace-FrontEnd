fe3ce21701ef73d9f6af49f48f4a433415d49139
cocoder16 <cocoder16@gmail.com>
2022-01-25 17:31:48 +0900

    [Update] policy test

--- cypress/integration/policy/navigatePolicy.spec.ts
+++ cypress/integration/policy/navigatePolicy.spec.ts
@@ -12,6 +12,18 @@ describe.only("navigate policy", () => {
 
     cy.url().should("eq", `${HOST_URL}/policy/contract`);
   });
+
+  it("When click 개인정보 처리방침 on footer, Then move to 개인정보 처리방침", () => {
+    cy.get("footer").contains("개인정보 처리방침").click();
+
+    cy.url().should("eq", `${HOST_URL}/policy/privacy`);
+  });
+
+  it("When click 취소 및 환불 정책 on footer, Then move to 취소 및 환불 정책", () => {
+    cy.get("footer").contains("취소 및 환불 정책").click();
+
+    cy.url().should("eq", `${HOST_URL}/policy/cancellation`);
+  });
 });
 
 export {};
