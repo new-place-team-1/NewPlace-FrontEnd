64af2d27b722d875cd7c7f628ba32aa210f99217
cocoder16 <cocoder16@gmail.com>
2022-02-05 17:28:26 +0900

    [Update] hide env in workflows

--- .github/workflows/scripts/env.sh
+++ .github/workflows/scripts/env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
-echo REACT_APP_HOST_URL=http://localhost:3000 >> .env
-echo REACT_APP_API_URL=http://localhost:8080 >> .env
+echo "REACT_APP_HOST_URL=$REACT_APP_HOST_URL" >> .env
+echo "REACT_APP_API_URL=$REACT_APP_API_URL" >> .env
